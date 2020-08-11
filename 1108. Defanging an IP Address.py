a11773676aab1abbdc16f6eed9035606a2782830
Aishwarya2411 <35129664+Aishwarya2411@users.noreply.github.com>
2020-08-11 14:00:09 -0400

    Create 1108. Defanging an IP Address.py

---
+++ 1108. Defanging an IP Address.py
@@ -0,0 +1,12 @@
+
+class Solution(object):
+    def defangIPaddr(self, address):
+        """
+        :type address: str
+        :rtype: str
+        """
+        new_address=address.replace('.','[.]')
+        
+        
+        return (new_address)
+        
