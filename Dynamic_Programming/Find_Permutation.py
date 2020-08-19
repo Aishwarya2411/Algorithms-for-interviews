368f293e519edfcd19aeccc251b439a3012cc958
Aishwarya2411 <aishwaryaamuthan@gmail.com>
2020-08-19 18:44:56 -0400

    find_permutation

---
+++ Dynamic_Programming/Find_Permutation.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findPermutation(self, s: str) -> List[int]:
+         
+         
+         """Keep adding values (1 to n) to a stack until char "I" is reached. Once "I" is reached empty the stack """
+         
+         stack=[]
+         res=[]
+         for i in range (len(s)):
+                
+                stack.append(i+1)
+                
+                if s[i]=='I':
+                    while stack:
+                       
+                        res.append(stack.pop())
+         
+         res.append(len(s)+1)     """appending the last element"""
+         while stack:             """If the last entry on the s is "D",                                         empting the stack here will sort the                                       last 2 elements"""
+                
+                res.append(stack.pop())
+                
+                
+                
+         return (res)
